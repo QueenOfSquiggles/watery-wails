b182d941933afa6b9fe8d7e1f1cab3a467d5e071
QueenOfSquiggles <8940604+QueenOfSquiggles@users.noreply.github.com>
2024-07-12 13:19:06 -0500

    Reorg into workspace for better module dev

---
+++ bevy_htnp/src/lib.rs
@@ -0,0 +1,5 @@
+use bevy_ecs::prelude::*;
+
+#[derive(Component)]
+/// Tagging component for marking an entity as an HTN Agent
+pub struct HtnAgent;
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,34 @@
+#![allow(dead_code)]
+
 use bevy::prelude::*;
-// use bevy_tweening::TweeningPlugin;
+use bevy_htnp::HtnAgent;
 use game::GamePlugin;
 use gen::GAME_VERSION;
-// use haalka::HaalkaPlugin;
 
 mod game;
 mod gen;
 
 fn main() {
-    info!("Watery Wails '{}' starting", GAME_VERSION);
     let mut app = App::new();
-    // #[cfg(target_family = "wasm")]
-    // {
-    //     // this helps WASM builds to work better especially with ItchIO, according to an official bevy example.
-    //     use bevy::asset::AssetMetaCheck;
-    //     app = app.insert_resource(AssetMetaCheck::Never);
-    // }
-    // #[cfg(not(target_family="wasm"))]
-    // {
-    // 	// include for all desktop builds
-    // 	//TODO: init bevy-panic-handler and steamworks
-    // }
+    #[cfg(target_family = "wasm")]
+    {
+        // this helps WASM builds to work better especially with ItchIO, according to an official bevy example.
+        use bevy::asset::AssetMetaCheck;
+        app = app.insert_resource(AssetMetaCheck::Never);
+    }
+    #[cfg(not(target_family = "wasm"))]
+    {
+        // include for all desktop builds
+        //TODO: init bevy-panic-handler and steamworks
+    }
     app.add_plugins(DefaultPlugins)
-        // .add_plugins(TweeningPlugin) // Tweens
-        // .add_plugins(HaalkaPlugin) // GUI layouting
         .add_plugins(GamePlugin) // game systems & data
+        .add_systems(Startup, startup_prints)
         .run();
+}
+
+fn startup_prints(mut commands: Commands) {
+    info!("Watery Wails '{}' starting", GAME_VERSION);
+
+    commands.spawn(HtnAgent);
 }
