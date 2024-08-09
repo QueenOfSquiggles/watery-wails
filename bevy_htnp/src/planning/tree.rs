852644a674e1e03994bd010d61a7589d8e63b443
QueenOfSquiggles <8940604+QueenOfSquiggles@users.noreply.github.com>
2024-08-09 14:49:30 -0500

    Converted to proper submodule workflow

--- bevy_htnp/src/planning/tree.rs
+++
@@ -1,8 +0,0 @@
-use std::sync::Arc;
-
-/// Super basic non-cyclic, directional graph (aka a tree)
-/// Uses Arc because I want/need it to be Send/Sync
-pub struct Node<T> {
-    pub value: T,
-    pub parent: Option<Arc<Node<T>>>, // god I wish there was a better ref-counted smart pointer that is Send/Sync
-}
